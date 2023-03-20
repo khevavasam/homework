f8446e940b62656d40fccb2cf14f26482cfff88b
Khevavasam Arthur <khevavasam@gmail.com>
2023-03-20 21:13:07 +0300

    Выполнил 13 задачу

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int Promt(string gg) 
+{
+    
+    System.Console.Write(gg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Promt ("Введите число: ");
+if (number < 100 )
+{
+    Console.WriteLine("В числе нету третьей цифры");
+    return;
+}
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+{
+Console.WriteLine(GetThirdRank(number));
+}
