2b3666a20e983bfcf4972a4eb491cf65ced56285
Khevavasam Arthur <khevavasam@gmail.com>
2023-03-20 19:42:28 +0300

    Выполнил 10 задачу

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Promt(string gg) 
+{
+    
+    System.Console.Write(gg);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Promt ("Введите трехзначное число: ");
+if (number < 100 || number > 999 )
+{
+    Console.WriteLine("Число которое вы ввели не является трехзначным");
+    return;
+}
+Console.WriteLine($"Введенное число {number}");
+int secondnumber = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {secondnumber}");
